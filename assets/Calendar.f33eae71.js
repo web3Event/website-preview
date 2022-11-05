a302dac92b5bfe526b5f9b7efe29873666a447b1
hominee <bruceyytsh@outlook.com>
2022-11-05 09:10:03 +0800

    website preview

---
+++ assets/Calendar.f33eae71.js
@@ -0,0 +1,1 @@
+import{o as e,c}from"./index.80c5fdc4.js";const n={class:"w-full h-full",src:"https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPng5669d183d41b7411ece762f664636df2b434a19802f213135e1c9d57330e3d33"},r={__name:"Calendar",props:{month:String,day:String},setup(s){return(t,o)=>(e(),c("img",n))}};export{r as _};
